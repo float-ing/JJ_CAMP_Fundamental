8380cd800b924ca589e3d7bb1f0359a912058d41
Jee Hoon <yamoo9@naver.com>
2017-03-04 19:03:06 +0900

    자료형 변환

--- LECTURE/DAY02/js/study.js
+++ LECTURE/DAY02/js/study.js
@@ -104,3 +104,99 @@ answer_btn.onclick = function() {
   // 수행할 일(function): 목표로 하는 단락 요소의 텍스트 값을 화면에 표시합니다.
   target_p.innerText = favorite_item + '. ' + current + ' 모델';
 };
+
+// --------------------------------------------
+// 선(후) 증가/감소 연산자
+// ++ / --
+
+var z = 10;
+
+z++; // "후증가" 현재 라인에서는 z 값이 변화하지 않는다. z = 10
+console.log('z =', z); // 다음 라인이기에 값은 변화된다. z = 11
+
+z--; // "후감소" 현재 라인에서는 z 값이 변화하지 않는다. z = 11
+console.log('z =', z); // 다음 라인이기에 값은 변화된다. z = 10
+
+++z; // "선증가" 현재 라인에서 즉시 z 값이 변화한다. z = 11
+console.log('z =', z); // 값은 이미 변화되어 그대로이다. z = 11
+
+--z; // "선감소" 현재 라인에서 즉시 z 값이 변화한다. z = 10
+console.log('z =', z); // 값은 이미 변화되어 그대로이다. z = 10
+
+// --------------------------------------------
+// 대입 연산자
+// =
+// +=
+// -=
+// *=
+// /=
+// %=
+
+var k = 99;
+
+// k = k + 10; // 99 + 10 = 109
+
+// 위 방법과 아래 방법은 결과적으로 동일하다.
+k += 10; // 99 + 10 = 109
+
+console.log('k:', k); // 109
+
+// +1 ?
+k = k + 1; // 110
+k += 1;    // 111
+k++;       // 111
+           // 112
+
+k = k - 1; // 111
+k -= 1;    // 110
+--k;       // 109
+
+// --------------------------------------------
+// 이스케이프(Escape) 처리, 백슬래시(\) 사용
+
+// var you_are = 'I'm a boy'; // ERROR
+var you_are = 'I\'m a boy'; // Escape 처리
+
+// var your_recored_is = 'my record is 18' 24".'; // ERROR
+var your_recored_is = 'my record is 18\' 24".'; // Escape 처리
+
+// --------------------------------------------
+// 논리(Boolean)
+// true | false
+
+// 숫자 중에서 오직 0만이 자동으로 Boolean 값을 바뀌었을 때 false가 된다.
+var type_casting_zero = Boolean(0); // false
+
+// 상태(State)를 기억하기 위한 변수는 일반적으로 Boolean 값을 담는다.
+// is_*
+// has_*
+var is_opened    = false;
+var has_children = true;
+
+// ------------------------------------------------
+// JavaScript 세상에서 객체가 아닌 2가지
+// undefined, null
+//
+// 이들은 객체가 아니기에 속성(Property)을 가질 수 없다.
+// [ERROR] cannot set property of null
+// [ERROR] cannot set property of undefined
+
+// 변수를 선언만 하고, 값을 할당하지 않은 초기 상태의 값은 undefined 이다.
+// 문서 객체의 이벤트 속성 초기 값은 모두 null 이다.
+// HTML 구조화한 요소들(Element, Tags(<element_name>))
+
+// 숫자 유형 -> 문자 유형으로 변경하는 4가지 방법
+// 1. '숫자' 문자화
+// 2. 숫자 + ''
+// 3. String(숫자)
+// 4. (숫자).toString()
+
+// 문자(숫자와 유사한) -> 숫자로 변경하는 방법
+// 1. -, *, / 등을 사용
+// 2. +문자(like number)
+// 3. Number(문자(like number))
+
+// 문자(숫자를 포함하는 문자(단위: px, em, ...)) -> 숫자 (문자(단위//) 제거)
+// 내장 함수(Built-in, Native)
+// parseInt('24px', 10) -> 24
+// parseFloat('18.34529px', 10) -> 18.34529
