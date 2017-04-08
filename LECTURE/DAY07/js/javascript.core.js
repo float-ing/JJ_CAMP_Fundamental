466389c32c5aeb3b48b9d2d008d193dcb3fae5fe
Jee Hoon <yamoo9@naver.com>
2017-04-08 16:48:37 +0900

    Bulma 버튼 셋 마크업 추가

--- LECTURE/DAY07/js/javascript.core.js
+++ LECTURE/DAY07/js/javascript.core.js
@@ -1,6 +1,7 @@
 /*! javascript.core.js © yamoo9.net, 2017 */
 
 
+// STEP 0
 // media/ 디렉토리 내부의 cover 이미지를
 // 사용자 화면(view)에 뿌리기 위해(순환 처리)
 // 배열(집합) 데이터를 모델링 하시오.
@@ -55,24 +56,28 @@ coverList.push({
 // 찾고자 하는 대상의 식별자를 사용하여 대상 찾기를 하자.
 var coverList_el = document.querySelector('.music-coverlist');
 // console.log('coverList_el:', coverList_el);
-var inner_html_code = '';
 
 // STEP 2
 // 데이터 coverList => HTML 템플릿에 순환 처리 하여 화면에 렌더링 => 뷰(View) 구현
-for ( var i=0, l=coverList.length; i<l; ++i ) {
-  // console.log(i);
-  // console.log(coverList[i]); // 커버 이미지 경로
-  var src = 'media/cover/' + coverList[i].src;
-  // console.log(src);
-  // coverList_el // <ul> <- <li>
-  inner_html_code += '<li class="music-cover-item">';
-  inner_html_code +=     '<img class="music-cover is-rwd" src="'+ src +'" alt="Katy Perry & Skip Marley - Chained To The Rhythm">';
-  inner_html_code += '</li>';
-}
-
-coverList_el.innerHTML += inner_html_code;
-
 // HTML Code: Template
 // <li class="music-cover-item">
 //   <img class="music-cover is-rwd" src="" alt="">
 // </li>
+
+// 순환 처리하여 완성할 HTML 코드를 담을 문자열 데이터
+var inner_html_code = '';
+
+for ( var i=0, l=coverList.length; i<l; ++i ) {
+  // console.log(i);
+  // console.log(coverList[i]); // 커버 이미지 경로
+  // [{src:"001.EdSheeran-ShapeOfYou.jpg", alt:""}]
+  var src = 'media/cover/' + coverList[i].src;
+  var alt = coverList[i].alt;
+  // console.log(src);
+  // coverList_el // <ul> <- <li>
+  inner_html_code += '<li class="music-cover-item">';
+  inner_html_code +=     '<img class="music-cover is-rwd" src="'+ src +'" alt="'+ alt +'">';
+  inner_html_code += '</li>';
+}
+
+coverList_el.innerHTML += inner_html_code;
