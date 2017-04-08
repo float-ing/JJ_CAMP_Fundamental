c8883029554d287d30ec0283d807cf0f053ff8af
Jee Hoon <yamoo9@naver.com>
2017-04-08 14:50:03 +0900

    커버리스트 데이터 모델링

--- LECTURE/DAY07/js/javascript.core.js
+++ LECTURE/DAY07/js/javascript.core.js
@@ -1,1 +1,22 @@
 /*! javascript.core.js © yamoo9.net, 2017 */
+
+
+// media/ 디렉토리 내부의 cover 이미지를
+// 사용자 화면(view)에 뿌리기 위해(순환 처리)
+// 배열(집합) 데이터를 모델링 하시오.
+
+var coverList = []; // Create Array
+
+// Add Item in Array
+coverList.push("001.EdSheeran-ShapeOfYou.jpg");
+coverList.push("002.TheChainsmokers&Halsey-Closer.jpg");
+coverList.push("003.Zayn&TaylorSwift-IDon'tWannaLiveForever(FiftyShadesDarker).jpg");
+coverList.push("004.KatyPerry&SkipMarley-ChainedToTheRhythm.jpg");
+coverList.push("005.Migos&LilUziVert-BadAndBoujee.jpg");
+coverList.push("006.TheChainsmokers-Paris.jpg");
+coverList.push("007.Rihanna-LoveOnTheBrain.jpg");
+coverList.push("008.BrunoMars-That'sWhatILike.jpg");
+coverList.push("009.BigSean-BounceBack.jpg");
+coverList.push("010.MachineGunKelly&CamilaCabello-BadThings.jpg");
+
+console.log('coverList:', coverList);
